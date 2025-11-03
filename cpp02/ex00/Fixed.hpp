f0012f5c8e5499cc3f33485eadd97ec3d6f6574c
asylum9 <agaland@student.s19.be>
2025-11-03 15:25:23 +0100

    bl

--- cpp02/ex00/Fixed.hpp
+++ cpp02/ex00/Fixed.hpp
@@ -8,7 +8,7 @@ class Fixed
 	private:
 
 	int	_fixedPointValue;
-	static const int _fractBits; //appartient a la classe et non a l'objet (commun a tous les objets)
+	static const int _fractBits = 8; //appartient a la classe et non a l'objet (commun a tous les objets)
 
 	public :
 
@@ -18,11 +18,8 @@ class Fixed
 	~Fixed(void);
 
 	Fixed&  operator=(Fixed const& rhs); //passe par defaut l'instance courante. Donc il y a 2 parametres en pratique et non 1.
-	//rhs = right hand side
 	int	getRawBits(void) const;
 	void setRawBits(int const raw);
 };
-
-static const int fractBits = 8;
 
 #endif
